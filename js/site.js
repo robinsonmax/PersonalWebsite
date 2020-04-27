2407d9ff34654cc1467e99f12466f5b15b333b19
Max Robinson <robinsonmax17@gmail.com>
2020-04-27 22:37:38 +0100

    Added scroll arrow

--- js/site.js
+++ js/site.js
@@ -2,9 +2,38 @@ const headerBlurElement = document.getElementById('header-blur');
 
 var scrollPercentage;
 
+
+window.onload = () => {
+    //window.onscroll();
+}
 window.onscroll = () => {
     scrollPercentage = window.scrollY / window.innerHeight;
     if(scrollPercentage <= 1) {
         headerBlurElement.style.opacity = scrollPercentage;
     }
+    fadeInOnScroll();
+}
+
+
+
+var fadeInElements = document.getElementsByClassName('fade-in-on-scroll');
+var fadeInElementsArray = Array.prototype.slice.call(fadeInElements,0);
+var fadedIn = []
+fadeInElementsArray.forEach(element => {
+    fadedIn[element] = false;
+});
+const fadeInOnScroll = () => {
+    fadeInElementsArray.forEach(element => {
+        if(!element.classList.contains('faded-in')){
+            var triggerPos = element.offsetTop - window.innerHeight;
+            if(window.scrollY > triggerPos){
+                element.classList.add('faded-in')
+                TweenMax.to(element, {
+                    y:-32,
+                    autoAlpha: 1,
+                    duration: 0.8
+                });
+            }
+        }
+    });
 }
