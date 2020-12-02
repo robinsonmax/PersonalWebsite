b0a90d86b33c1422764d072326db0b0c4964738c
Max Robinson <robinsonmax17@gmail.com>
2020-12-02 16:26:25 +0000

    Made scrollbar dragable

--- src/ScrollBar.js
+++ src/ScrollBar.js
@@ -10,12 +10,33 @@ export default class ScrollBar extends Component {
             const totalHeight = scrollHeight - windowHeight;
             document.getElementById("scrollbar-thumb").style.top = `${100*window.scrollY/totalHeight}%`
         })
+        const thumb = document.getElementById("scrollbar-thumb")
+        var mouseDown = false;
+        var yOffset = 0;
+        thumb.addEventListener("mousedown", (event) => {
+            const thumbPos = thumb.getBoundingClientRect()
+            yOffset = event.clientY - thumbPos.y - (thumbPos.height / 2)
+            mouseDown = true
+        })
+        window.addEventListener("mouseup", (event) => {
+            mouseDown = false
+        })
+        window.onmousemove = (event) => {
+            if(mouseDown){
+                const padding = 45
+                const prog = (event.clientY - yOffset - padding)
+                const total = (window.innerHeight - (padding*2))
+                const scrollableDistance = document.body.scrollHeight - window.innerHeight
+                window.scrollTo(0,(scrollableDistance*prog/total))
+            }
+        }
+
     }
 
     render(){
         return (
             <div className="scrollbar">
-                <span id="scrollbar-thumb" className="scrollbar-thumb"></span>
+                <input type="textbox" id="scrollbar-thumb" className="scrollbar-thumb"/>
                 <span className="scrollbar-track"></span>
             </div>
         );
