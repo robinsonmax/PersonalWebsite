efb6204a2e45c44c66f6e4b76f68bd9af512d559
Max Robinson <robinsonmax17@gmail.com>
2020-12-02 21:35:56 +0000

    Added text

--- src/App.js
+++ src/App.js
@@ -11,9 +11,39 @@ export default function App() {
 
         <Header/>
 
-        <div style={{height: "3000px"}}>
-        </div>
-
+        <section>
+          <h1>About</h1>
+          <p>Hi, I'm Max and I'm a Software Developer with experience of modern web technologies in the information technology industry. Driven by solving complex problems, I'm skilled in ASP.Net, JavaScript, SQL as well as many front-end web technologies.</p>
+        </section>
+        <section>
+          <h1>Skills</h1>
+          <ul>
+            <li>
+              <strong>JavaScript & React</strong>
+              <p>I have experience with React, jQuery and vanilla JavaScript for creating rich front-end user experiences, as well working with NodeJS & Express for creating basic Web APIs.</p>
+            </li>
+            <li>
+              <strong>ASP.Net MVC & WebForms (C#)</strong>
+              <p>I can competently create web apps & REST APIs using both ASP.Net core and framework.</p>  
+            </li>
+            <li>
+              <strong>SQL</strong>
+              <p>TEXT</p>  
+            </li>
+            <li>
+              <strong>Python</strong>
+              <p>TEXT</p>  
+            </li>
+          </ul>
+        </section>
+        <section>
+          <h1>Tracking app for Warrington scouts</h1>
+          <p>TEXT</p>
+        </section>
+        <section>
+          <h1>Contact</h1>
+          <p>TEXT</p>
+        </section>
 
         <footer>
           <p>© 2020 Max Robinson</p>
