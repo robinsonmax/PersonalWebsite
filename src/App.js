0817a8f1d795bd81d43b6768a5bf3548b58ff3ff
Max Robinson <robinsonmax17@gmail.com>
2020-12-03 13:05:10 +0000

    Added text

--- src/App.js
+++ src/App.js
@@ -7,48 +7,60 @@ export default function App() {
 
   return (
     <Background>
-        <ScrollBar/>
+      <ScrollBar/>
 
-        <Header/>
+      <Header/>
 
-        <section>
-          <h1>About</h1>
-          <p>REWRITE THIS BIT!!!</p>
-          <p>Hi, I'm Max and I'm a Software Developer with experience of modern web technologies in the information technology industry. Driven by solving complex problems, I'm skilled in ASP.Net, JavaScript, SQL as well as many front-end web technologies.</p>
-        </section>
-        <section>
-          <h1>Skills</h1>
-          <ul>
-            <li>
-              <strong>JavaScript & React</strong>
-              <p>I have experience with React, jQuery and vanilla JavaScript for creating rich front-end user experiences, as well working with NodeJS & Express for creating basic Web APIs.</p>
-            </li>
-            <li>
-              <strong>ASP.Net MVC & WebForms (C#)</strong>
-              <p>I can competently create web apps & REST APIs using both ASP.Net core and framework.</p>  
-            </li>
-            <li>
-              <strong>SQL</strong>
-              <p>TEXT</p>  
-            </li>
-            <li>
-              <strong>Python</strong>
-              <p>TEXT</p>  
-            </li>
-          </ul>
-        </section>
-        <section>
-          <h1>Tracking app for Warrington scouts</h1>
-          <p>TEXT</p>
-        </section>
-        <section>
-          <h1>Contact</h1>
-          <p>TEXT</p>
-        </section>
+      <section>
+        <h1>About</h1>
+        <p>Hi, I'm Max and I'm a Junior Software Developer with experience of modern web technologies in the information technology industry. Driven by solving complex problems, I'm skilled in ASP.Net, JavaScript, SQL as well as many front-end web technologies.</p>
+      </section>
+      <section>
+        <h1>Skills</h1>
+        <ul>
+          <li>
+            <strong>JavaScript & React</strong>
+            <p>I have experience with React, jQuery and vanilla JavaScript for creating rich front-end user experiences, as well working with NodeJS & Express for creating basic Web APIs.</p>
+          </li>
+          <li>
+            <strong>ASP.Net MVC & WebForms (C#)</strong>
+            <p>I can competently create web apps & REST APIs using both ASP.Net Core and Framework.</p>  
+          </li>
+          <li>
+            <strong>SQL</strong>
+            <p>I can comfortably write SQL queires & stored procedures, as well as designing databases, having experience with Microsoft SQL Server. Recently, I have been getting to know Firebase's NOSQL database for my smaller projects.</p>  
+          </li>
+          <li>
+            <strong>Design Tools</strong>
+            <p>I use Adobe XD & Illustrator to create designs and graphics for my projects. Recently, I have been using them in conjunction with Rive2 to create animated graphics.</p>
+          </li>
+          <li>
+            <strong>Cloud</strong>
+            <p>I have a basic understanding of Google cloud & Microsoft azure, having used them both to host various web apps & databases.</p>
+          </li>
+          <li>
+            <strong>Python</strong>
+            <p>I use Python for creating simple scripts to automate common tasks.</p>  
+          </li>
 
-        <footer>
-          <p>© 2020 Max Robinson</p>
-        </footer>
+        </ul>
+      </section>
+      <section>
+        <h1>Technical Interests</h1>
+        <p>In the last year or so, I have taken a greater interest in front-end development, and creating modern user interfaces.</p>
+      </section>
+      <section>
+        <h1>Tracking app for Warrington scouts</h1>
+        <p>TEXT</p>
+      </section>
+      <section>
+        <h1>Contact</h1>
+        <p>TEXT</p>
+      </section>
+
+      <footer>
+        <p>© 2020 Max Robinson</p>
+      </footer>
 
     </Background>
   );
