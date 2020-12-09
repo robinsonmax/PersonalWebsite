c36011735e752a545f386cad33a7e640f46690aa
Max Robinson <robinsonmax17@gmail.com>
2020-12-09 23:06:55 +0000

    Dankified rick

--- src/Rick.js
+++ src/Rick.js
@@ -1,20 +1,13 @@
 import RickGif from './img/rick.gif';
 
-const divCss = { 
-    textAlign: "center",
-    width: "100%",
-    height: "100%"
-}
-
 const imageCss = {
-    height: "100vh"
+    height: "100vh",
+    width: "100vw"
 }
 
 export default function Rick() {
 
   return (
-      <div style={divCss}>
-          <img src={RickGif} style={imageCss}></img>
-      </div>
+        <img src={RickGif} style={imageCss}></img>
   )
 }
